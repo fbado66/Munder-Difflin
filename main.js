00e4caf7c65b0e8eccbf2a068f2a66893f67b951
Franklin Bado <fbado66@gmail.com>
2018-08-08 17:58:52 -0400

    first draft

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+// window.onscroll = function() {myFunction()};
+
+// let mynavbar = document.getElementById("navbar");
+// let sticky = mynavbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     mynavbar.classList.add("sticky");
+//   } else {
+//     mynavbar.classList.remove("sticky");
+//   }
+// }
